66aaa3ce82c3e92fe761d4eef22724c2782e8ff7
jaylanEsquivel <jaylan1997@hotmail.com>
2017-10-19 17:08:28 -0300

    v1.0

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--
+NOME: Template Base;
+DESCRIÇAO: Template base para Projetos;
+AUTOR: Jaylan Esquivel Pereira Santos;
+TECNOLOGIAS DISPONIVEIS: Bootstrap, Jquery, Animate.css, FontAwesome, Normalize.css, MaskJquery;
+-->
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/> <!-- ESSA META TIRA ZOOM EM DISPOSITIVOS MOBILES -->
+        <meta charset="UTF-8">
+        <title>## PROJETO BASE ##</title>
+        <link rel="stylesheet" href="css/animate.css" />
+        <link rel="stylesheet" href="css/normalize.css" />
+        <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css" />
+        <link rel="stylesheet" href="css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="css/style-one.css"/>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    
+                </div>
+            </div>
+        </div>       
+<!--======================================= js  =========================================== -->
+<script src="js/jquery-3.2.1.min.js" ></script> 
+<script src="js/bootstrap.min.js"></script>
+<script src="js/anijs.js" type="text/javascript"></script> 
+<script src="js/anijs-helper-scrollreveal.js" type="text/javascript"></script> 
+<!--======================================= js  =========================================== -->
+    </body>
+</html>
